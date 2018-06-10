23d8226b481fb965b59dbada8b29b5282c9fbac3
Andrzej Salak <andrzej.salak@gmail.com>
2018-06-10 22:07:33 +0200

    Read file sync

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ var fs = require('fs');
 var path = require('path');
 var memwatch = require('memwatch-next');
 
-memwatch.on('stats', function(stats) {
-// do something with post-gc memory usage stats
-});
-
 function readFile() {
-    var fileContent = fs.readFile(path.resolve('file-to-read.txt'), function (err, buffer) {
-        // console.log("" + content);
-        var content = buffer;
-        content = null;
+    fs.readFile(path.resolve('file-to-read.txt'), function (err, buffer) {
+        buffer = null;
     });
 }
 
+function readFileSync() {
+    var fileContent = fs.readFileSync(path.resolve('file-to-read.txt'));
+}
 
-memwatch.on('stats', function(stats) { console.log(stats.estimated_base, stats.current_base); });
+memwatch.on('stats', function(stats) { 
+    console.log(stats.estimated_base, stats.current_base); 
+});
 
 for(var i = 0; i < 1000000; i++) {
-    readFile()
+    readFileSync();
     if (i === 500000) {
         console.log("run gc");
         global.gc();
-
     }
 }
 
+global.gc();
