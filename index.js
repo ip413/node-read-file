b18da82f3aa854e1d7b30b129ed239547ffc3bee
Andrzej Salak <andrzej.salak@gmail.com>
2018-06-13 06:20:42 +0200

    memwatch on memory leak + process info

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 var memwatch = require('memwatch-next');
+var printProcessData = false;
+var LOOP_LIMIT = 1000000;
+var HALF_LOOP_LIMIT = LOOP_LIMIT/2;
 
 function readFile() {
     fs.readFile('file-to-read.txt', function (err, content) {
@@ -13,15 +16,28 @@ function readFileSync() {
 }
 
 memwatch.on('stats', function(stats) { 
-    console.log(stats.estimated_base, stats.current_base); 
+    console.log(stats.estimated_base, stats.current_base);
+    // printProcessData && console.log(process.memoryUsage());
 });
 
-for(var i = 0; i < 1000000; i++) {
+memwatch.on('leak', (info) => {
+  console.error('memory leak:\n', info);
+});
+
+for(var i = 0; i < LOOP_LIMIT; i++) {
     readFile();
     // readFileSync();
-    if (i === 500000) {
+    if (i === HALF_LOOP_LIMIT) {
         console.log("run gc");
         global.gc();
+    }
+
+    if (i === LOOP_LIMIT - 1) {
+        setTimeout(function () {
+            console.log("almost finished");
+            global.gc();
+            printProcessData = true;
+        }, 12000);
     }
 }
 
