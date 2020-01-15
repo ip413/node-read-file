b1f1992600e783444acf103de57a82fe68776587
as <andrzej.salak@gmail.com>
2020-01-15 12:37:38 +0100

    memwatch removed

---
+++ README.md
@@ -0,0 +1,19 @@
+# Test for memory management of fs module, for async and sync read
+* Memory could be measured with:
+
+```watch -n 1 'pmap -p \$(pgrep node) | tail -n -1 | cut -c 10- | tee -a result.txt'```
+
+or
+
+```top | watch node```
+
+* Run test with:
+
+```npm start```
+
+* Test has no npm dependencies.
+
+
+* Results for node 6 and 12:
+
+![](https://github.com/ip413/node-read-file/blob/master/docs/data.png)
--- index.js
+++ index.js
@@ -1,42 +1,35 @@
 var fs = require('fs');
-var memwatch = require('memwatch-next');
-var printProcessData = false;
-var LOOP_LIMIT = 1000000;
+var LOOP_LIMIT = 10e5;
 var HALF_LOOP_LIMIT = LOOP_LIMIT/2;
 
 function readFile() {
-    fs.readFile('file-to-read.txt', function (err, content) {
-        // console.log("" + content);
+    fs.readFile('file-to-read.txt', function (err, _content) {
+        // console.log("" + _content.length);
+        // = undefined; = null; delete - all those have no impact on memory usage here 
+        var content = _content;
     });
 }
 
 function readFileSync() {
+    // console.log("" + _content.length);
+    // = undefined; = null; delete - all those have no impact on memory usage here 
     var content = fs.readFileSync('file-to-read.txt');
-    // console.log("" + content);
 }
 
-memwatch.on('stats', function(stats) { 
-    console.log(stats.estimated_base, stats.current_base);
-    // printProcessData && console.log(process.memoryUsage());
-});
-
-memwatch.on('leak', (info) => {
-  console.error('memory leak:\n', info);
-});
-
 for(var i = 0; i < LOOP_LIMIT; i++) {
-    readFile();
-    // readFileSync();
+    // readFile();
+    readFileSync();
     if (i === HALF_LOOP_LIMIT) {
         console.log("run gc");
         global.gc();
     }
 
     if (i === LOOP_LIMIT - 1) {
+        global.gc();
+        console.log("almost finished");
         setTimeout(function () {
-            console.log("almost finished");
             global.gc();
-            printProcessData = true;
+            console.log("finished");
         }, 12000);
     }
 }
